458771ef1a3dc6bb0cb8b1b55256ea751ad853e7
iliasradouche <iliasradouche2@gmail.com>
2025-07-20 16:31:04 +0100

    First test

---
+++ alx_travel_app/alx_travel_app_0x00/alx_travel_app/listings/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Listing, Booking
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = [
+            'id',
+            'title',
+            'description',
+            'location',
+            'price_per_night',
+            'owner',
+            'created_at',
+        ]
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = [
+            'id',
+            'listing',
+            'user',
+            'check_in',
+            'check_out',
+            'guests',
+            'created_at',
+        ]
